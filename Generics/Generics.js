5509d785387604f67c998545ca62b080bfdd7ec4
Edwin Calle <calleojeda4@gmail.com>
2023-10-13 18:47:42 -0500

    Generics con TypeScript arrays

---
+++ Generics/Generics.js
@@ -0,0 +1,5 @@
+"use strict";
+//Generics
+let arregloNumeros;
+arregloNumeros = [1, 2, 3, 4];
+console.log(arregloNumeros);
---
+++ Generics/Generics.ts
@@ -0,0 +1,4 @@
+//Generics
+let arregloNumeros: Array<number>;
+arregloNumeros = [1, 2, 3, 4];
+console.log(arregloNumeros);
