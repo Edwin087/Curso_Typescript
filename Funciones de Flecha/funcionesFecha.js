8bea1e818f3a1bb2d2a8f7fadaf869fa96938ae4
Edwin Calle <calleojeda4@gmail.com>
2023-10-13 20:38:24 -0500

    Funciones de Flecha con TypeScript

---
+++ Funciones de Flecha/funcionesFecha.js
@@ -0,0 +1,10 @@
+"use strict";
+let suma = function (a, b) {
+    return a + b;
+};
+console.log(suma(3, 2));
+//let sumaFlecha = (a: number, b: number) => a + b;
+let sumaFlecha = (a, b) => {
+    return a + b;
+};
+console.log(sumaFlecha(3, 4));
---
+++ Funciones de Flecha/funcionesFecha.ts
@@ -0,0 +1,10 @@
+let suma = function (a: number, b: number) {
+  return a + b;
+};
+console.log(suma(3, 2));
+//let sumaFlecha = (a: number, b: number) => a + b;
+
+let sumaFlecha = (a: number, b: number) => {
+  return a + b;
+};
+console.log(sumaFlecha(3, 4));
